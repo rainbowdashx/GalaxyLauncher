c112131fb68e7714a3660708a1823603635215e6
rainbowdashx <born_to_kill_666@hotmail.com>
2017-09-03 18:05:17 +0200

    initial

---
+++ GalaxyLauncher/MainWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace GalaxyLauncher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public static ProgressBar Downloadbar;
+        private bool RepairMode = false;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+
+
+        private async void btnDownload_Click(object sender, RoutedEventArgs e)
+        {
+            btnDownload.IsEnabled = false;
+
+            String oldVersionString = "0.0.0";
+            String newVersionString = StreamHelper.ReadFile(StreamHelper.gameVersionFilePathOnline);
+
+            try
+            {
+                oldVersionString = File.ReadAllText(System.IO.Path.Combine(Environment.CurrentDirectory, StreamHelper.gameVersionFilePathLocal));
+            }
+            catch (Exception ex)
+            {
+                oldVersionString = "0.0.0";
+                Console.WriteLine(ex.Message);
+            }
+            if (RepairMode)
+            {
+                oldVersionString = "0.0.0";
+                RepairMode = false;
+            }
+
+            Version newVersion = new Version(newVersionString);
+            Version oldVersion = new Version(oldVersionString);
+
+
+            int isNewer = newVersion.CompareTo(oldVersion);
+
+
+
+            if (isNewer > 0)
+            {
+                String data = StreamHelper.ReadFile(StreamHelper.manifestFileOnline);
+                List<string> lines = data.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+
+
+                
+
+                var progress = new Progress<int>(percent =>
+                {
+                    pgbDownload.Value = percent;
+                });
+
+                pgbDownload.Value = 0;
+                pgbDownload.Maximum = lines.Count;
+                pgbDownload.Foreground = Brushes.Orange;
+                lines = await Task.Run(() => StreamHelper.CheckGameFiles(lines, progress));
+
+                pgbDownload.Value = 0;
+                pgbDownload.Maximum = lines.Count;
+                pgbDownload.Foreground = Brushes.Green;
+                await Task.Run(() => StreamHelper.DownloadGameFiles(lines, progress));
+
+
+            }
+
+            btnDownload.IsEnabled = true;
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = System.IO.Path.Combine(Environment.CurrentDirectory, StreamHelper.gameExecutable);
+            process.EnableRaisingEvents = true;
+            process.Start();
+
+            process.Exited += (sender_, e_) =>
+            {
+                int code = process.ExitCode;
+                if (code != 0)
+                {
+                    RepairMode = true;
+                }
+                process.Dispose();
+            };
+
+
+
+        }
+    }
+}
---
+++ GalaxyLauncher/StreamHelper.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaxyLauncher
+{
+    public static class StreamHelper
+    {
+
+        public static Uri host = new Uri("ftp://h2412564.stratoserver.net/");
+        public static String gameVersionFilePathOnline = "/game/Win64/bin/GameVersion.txt";
+        public static String gameVersionFilePathLocal = "game/GameVersion.txt";
+        public static String manifestFileOnline = "/game/Win64/GameManifest.txt";
+        public static String gameExecutable = "game/dummyWoW.exe";
+
+        public static String ReadFile(String path)
+        {
+            FtpWebRequest ftp = (FtpWebRequest)FtpWebRequest.Create(new Uri(host, path));
+
+            FtpWebResponse response = (FtpWebResponse)ftp.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            string data = reader.ReadToEnd();
+            Console.WriteLine(data);
+
+            response.Close();
+            responseStream.Close();
+
+            return data;
+
+        }
+
+
+        public static void DownloadFile(String path)
+        {
+#if DEBUG
+
+            System.Threading.Thread.Sleep(1000);
+
+
+#else
+            FtpWebRequest ftp = (FtpWebRequest)FtpWebRequest.Create(new Uri(host, "game/Win64/bin/" + path));
+
+            String filePath = string.Format("{0}game{1}", AppDomain.CurrentDomain.BaseDirectory, path);
+            EnsureFolder(filePath);
+
+            using (Stream ftpStream = ftp.GetResponse().GetResponseStream())
+            using (Stream fileStream = File.Create(filePath))
+            {
+                ftpStream.CopyTo(fileStream);
+            }
+#endif
+        }
+
+        public static Task<List<String>> CheckGameFiles(List<String> fileList, IProgress<int> progress)
+        {
+
+            int FileNum = 0;
+            foreach (var item in fileList)
+            {
+                String[] fileString = item.Split(':');
+                String fileName = fileString[0];
+                String fileHash = fileString[1];
+                String fileSize = fileString[2];
+
+                String filePath = string.Format("{0}game{1}", AppDomain.CurrentDomain.BaseDirectory, fileName);
+
+                List<String> removeList = new List<string>();
+                if (File.Exists(filePath) && GetChecksum(filePath).Equals(fileHash))
+                {
+                    removeList.Add(item);
+                }
+                fileList = fileList.Except(removeList).ToList();
+                FileNum++;
+                if (progress != null)
+                {
+                    progress.Report(FileNum);
+                }
+            }
+            return Task.FromResult(fileList);
+        }
+
+        public static Task DownloadGameFiles(List<String> fileList, IProgress<int> progress)
+        {
+
+            int FileNum = 0;
+            foreach (var item in fileList)
+            {
+                String[] fileString = item.Split(':');
+                String fileName = fileString[0];
+                String fileHash = fileString[1];
+                String fileSize = fileString[2];
+                Console.WriteLine(fileName);
+                DownloadFile(fileName);
+                FileNum++;
+                if (progress != null)
+                {
+                    progress.Report(FileNum);
+                }
+            }
+            return Task.FromResult(0);
+        }
+
+
+        private static void EnsureFolder(string path)
+        {
+            string directoryName = Path.GetDirectoryName(path);
+            if ((directoryName.Length > 0) && (!Directory.Exists(directoryName)))
+            {
+                Directory.CreateDirectory(directoryName);
+                Console.WriteLine("Folder Created");
+            }
+        }
+        private static String GetChecksum(String path)
+        {
+
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(path))
+                {
+
+                    String zero = String.Empty;
+                    String hash = BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", zero);
+                    return hash;
+                }
+            }
+        }
+    }
+}
